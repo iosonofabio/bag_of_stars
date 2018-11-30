8c4a92ba535285dde7f4c5853e99763e4547239c
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-11-30 14:01:38 -0800

    Skip loading STAR genome in batch job if not needed

--- bos/bag_of_stars_job.py
+++ bos/bag_of_stars_job.py
@@ -21,6 +21,19 @@ def printfl(*args):
     return print(*args, flush=True)
 
 
+class BagOfStars(object):
+    def __init__(self, args):
+        self.args = args
+
+    def check_need_star_genome(self):
+        self.need_star = False
+        for sn, (fq1, fq2) in zip(self.group, self.group_fastq):
+            flag_fn = self.args.output+sn+'/STAR.done'
+            if not os.path.isfile(flag_fn):
+                self.need_star = True
+                return
+
+
 if __name__ == '__main__':
 
     pa = argparse.ArgumentParser(description='STAR + htseq-count in bags')
@@ -72,12 +85,15 @@ if __name__ == '__main__':
     if args.chain_htseq and (args.annotationFile is None):
         raise ValueError('To run htseq-count, specify --annotationFile')
 
+    bos = BagOfStars(args)
+
     args.output = args.output.rstrip('/')+'/'
     args.samplenames = args.samplenames.strip("'").split(' ')
     args.fastqs = args.fastqs.strip("'").split(' ')
-    group = args.samplenames
-    group_fastq = list(zip(args.fastqs[::2], args.fastqs[1::2]))
     output_tmp_star = args.output+'STAR_TMP_group_{:}/'.format(args.group_name)
+
+    bos.group = args.samplenames
+    bos.group_fastq = list(zip(args.fastqs[::2], args.fastqs[1::2]))
 
     printfl('Remove previous STAR temp dir')
     if not args.dry:
@@ -85,94 +101,98 @@ if __name__ == '__main__':
                check=True,
                shell=True)
 
-    printfl('Load genome into memory')
-    call = [
-        os.getenv('STAR', 'STAR'),
-        '--genomeDir', args.genomeDir,
-        '--genomeLoad', 'LoadAndExit',
-        '--outTmpDir', output_tmp_star,
-        ]
-    printfl(' '.join(call))
-    if not args.dry:
-        sp.run(call, check=True)
+    if bos.need_star:
+        printfl('Load genome into memory')
+        call = [
+            os.getenv('STAR', 'STAR'),
+            '--genomeDir', args.genomeDir,
+            '--genomeLoad', 'LoadAndExit',
+            '--outTmpDir', output_tmp_star,
+            ]
+        printfl(' '.join(call))
+        if not args.dry:
+            sp.run(call, check=True)
 
-    try:
-        for sn, (fq1, fq2) in zip(group, group_fastq):
+        try:
+            for sn, (fq1, fq2) in zip(bos.group, bos.group_fastq):
+                printfl('Remove previous STAR temp dir')
+                if not args.dry:
+                    sp.run('rm -rf {:}'.format(output_tmp_star),
+                           check=True,
+                           shell=True)
+
+                printfl('Mapping sample {:}'.format(sn))
+                flag_fn = args.output+sn+'/STAR.done'
+                if os.path.isfile(flag_fn):
+                    printfl('Flag file found, skipping sample')
+                    continue
+                call = [
+                    os.getenv('STAR', 'STAR'),
+                    '--runMode', 'alignReads',
+                    '--genomeDir', args.genomeDir,
+                    '--genomeLoad', 'LoadAndKeep',
+                    '--outTmpDir', output_tmp_star,
+                    '--readFilesIn', fq1, fq2,
+                    '--readFilesCommand', 'zcat',
+                    '--outFilterType', 'BySJout',
+                    '--outFilterMultimapNmax', '20',
+                    '--alignSJoverhangMin', '8',
+                    '--alignSJDBoverhangMin', '1',
+                    '--outFilterMismatchNmax 999',
+                    '--outFilterMismatchNoverLmax 0.04',
+                    '--alignIntronMin', '20',
+                    '--alignIntronMax', '1000000',
+                    '--alignMatesGapMax', '1000000',
+                    '--outSAMstrandField', 'intronMotif',
+                    '--outFileNamePrefix', args.output+sn+'/',
+                    '--outSAMtype', 'BAM', 'Unsorted',
+                    '--outSAMattributes', 'NH', 'HI', 'AS', 'NM',
+                               '--outFilterMatchNminOverLread', '0.4',
+                               '--outFilterScoreMinOverLread', '0.4',
+                               '--clip3pAdapterSeq', 'CTGTCTCTTATACACATCT',
+                               '--outReadsUnmapped', 'Fastx',
+                               ]
+                printfl(' '.join(call))
+                if not args.dry:
+                    sp.run(call, check=True)
+                    Path(flag_fn).touch()
+
+        finally:
             printfl('Remove previous STAR temp dir')
             if not args.dry:
                 sp.run('rm -rf {:}'.format(output_tmp_star),
                        check=True,
                        shell=True)
 
-            printfl('Mapping sample {:}'.format(sn))
-            flag_fn = args.output+sn+'/STAR.done'
-            if os.path.isfile(flag_fn):
-                printfl('Flag file found, skipping sample')
-                continue
+            printfl('Remove genome from memory')
             call = [
                 os.getenv('STAR', 'STAR'),
-                '--runMode', 'alignReads',
                 '--genomeDir', args.genomeDir,
-                '--genomeLoad', 'LoadAndKeep',
+                '--genomeLoad', 'Remove',
                 '--outTmpDir', output_tmp_star,
-                '--readFilesIn', fq1, fq2,
-                '--readFilesCommand', 'zcat',
-                '--outFilterType', 'BySJout',
-                '--outFilterMultimapNmax', '20',
-                '--alignSJoverhangMin', '8',
-                '--alignSJDBoverhangMin', '1',
-                '--outFilterMismatchNmax 999',
-                '--outFilterMismatchNoverLmax 0.04',
-                '--alignIntronMin', '20',
-                '--alignIntronMax', '1000000',
-                '--alignMatesGapMax', '1000000',
-                '--outSAMstrandField', 'intronMotif',
-                '--outFileNamePrefix', args.output+sn+'/',
-                '--outSAMtype', 'BAM', 'Unsorted',
-                '--outSAMattributes', 'NH', 'HI', 'AS', 'NM',
-                           '--outFilterMatchNminOverLread', '0.4',
-                           '--outFilterScoreMinOverLread', '0.4',
-                           '--clip3pAdapterSeq', 'CTGTCTCTTATACACATCT',
-                           '--outReadsUnmapped', 'Fastx',
-                           ]
+                ]
             printfl(' '.join(call))
             if not args.dry:
-                sp.run(call, check=True)
-                Path(flag_fn).touch()
+                try:
+                    sp.run(call, check=True)
+                except sp.CalledProcessError as e:
+                    print(e)
+                    print('Moving on')
 
-    finally:
-        printfl('Remove previous STAR temp dir')
-        if not args.dry:
-            sp.run('rm -rf {:}'.format(output_tmp_star),
-                   check=True,
-                   shell=True)
+            printfl('Remove previous STAR temp dir')
+            if not args.dry:
+                sp.run('rm -rf {:}'.format(output_tmp_star),
+                       check=True,
+                       shell=True)
 
-        printfl('Remove genome from memory')
-        call = [
-            os.getenv('STAR', 'STAR'),
-            '--genomeDir', args.genomeDir,
-            '--genomeLoad', 'Remove',
-            '--outTmpDir', output_tmp_star,
-            ]
-        printfl(' '.join(call))
-        if not args.dry:
-            try:
-                sp.run(call, check=True)
-            except sp.CalledProcessError as e:
-                print(e)
-                print('Moving on')
-
-        printfl('Remove previous STAR temp dir')
-        if not args.dry:
-            sp.run('rm -rf {:}'.format(output_tmp_star),
-                   check=True,
-                   shell=True)
+    else:
+        printfl('All samples in this group are mapped already, no need for STAR.')
 
     if args.htseq:
         print('STAR mappings done, check output BAM files')
-        mapped_fns = [args.output+sn+'/Aligned.out.bam' for sn in group]
+        mapped_fns = [args.output+sn+'/Aligned.out.bam' for sn in bos.group]
         has_failed = False
-        for sn, mapped_fn in zip(group, mapped_fns):
+        for sn, mapped_fn in zip(bos.group, mapped_fns):
             print(os.path.dirname(mapped_fn)+'...', end='', flush=True)
             try:
                 with pysam.AlignmentFile(mapped_fn, 'rb') as bamfile:
@@ -209,7 +229,7 @@ if __name__ == '__main__':
         if not args.dry:
             output = sp.run(call, check=True, stdout=sp.PIPE).stdout.decode()
             with open(htseq_fn, 'wt') as fout:
-                fout.write('\t'.join(['feature'] + group)+'\n')
+                fout.write('\t'.join(['feature'] + bos.group)+'\n')
                 fout.write(output)
 
     if args.chain_htseq is not None:
@@ -233,7 +253,7 @@ if __name__ == '__main__':
         htseq_fn = args.output+'counts.tsv'
         if not args.dry:
             samplenames = []
-            n_samples_max = n_groups * len(group)
+            n_samples_max = n_groups * len(bos.group)
             first_group = True
             for ig, counts_fn in enumerate(counts_fns):
                 print('Reading counts for group {:}'.format(ig+1))
